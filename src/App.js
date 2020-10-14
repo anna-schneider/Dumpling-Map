edc69026f24d22352ac15fa2392d980630c31f8e
Anna Schneider <anna.schneiderm@gmail.com>
2020-10-14 12:53:15 -0400

    Initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import "./App.css"
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">Dumpling Map</header>
+		</div>
+	)
+}
+
+export default App
