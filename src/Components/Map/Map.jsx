e6f96cc2a1ec85a592d7c94ae69567cdb37ed7df
Anna Schneider <anna.schneiderm@gmail.com>
2020-10-16 12:50:22 -0400

    Custom mapbox style rendering

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,29 +1,36 @@
-import React, { useEffect, useRef } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import mapboxgl from "mapbox-gl"
 
 import "./Map.css"
 
 mapboxgl.accessToken="pk.eyJ1IjoiYm5hbiIsImEiOiJja2c5bjZjbzIwOXJkMnJxZDA5eGY4eDA1In0.v4-4SGcs4DRis32p0bBuIA"
 
-const Map = ({map}) => {
-  const mapContainerRef = useRef()
+export default function MapDetail() {
+  const mapContainerRef = useRef(null)
+
+  const [lng, setlng] = useState(5)
+  const [lat, setLat] = useState(34)
+  const [zoom, setZoom] = useState(2)
+
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [40.60924, -74.237],
-      zoom: 9,
-    }) 
-  },[])
+      style: "mapbox://styles/bnan/ckg9o2hi702bc19kw4lsy82ip",
+      center: [lng, lat],
+      zoom: zoom,
+    })
+    
+  })
+  
+  
    
 
   return (
     <>
       <div className="map-container" ref={mapContainerRef}>
-{map}
+
       </div>
     </>
   )
 }
 
-export default Map
